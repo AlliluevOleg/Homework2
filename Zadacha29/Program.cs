e698021a7c67844ac398a563c6f1d5d931a5329b
Oleg <Oleg.evpator@mail.ru>
2022-09-16 09:29:06 +0300

    add Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,13 @@
+﻿int size = 8;
+int[] array = new int [size];
+Fillarray(array);
+
+System.Console.WriteLine("[{0}]", string.Join(", ", array));
+
+void Fillarray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(1, 100);
+    }
+}
