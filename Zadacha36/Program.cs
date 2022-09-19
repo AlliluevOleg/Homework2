9e42858aeab57f337a5f1703a013554f3831ac3c
Oleg <Oleg.evpator@mail.ru>
2022-09-19 11:29:20 +0300

    add Zadacha36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,28 @@
+﻿void Fillarray(int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 99);
+    }
+}
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SummNech(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+int size = DataEntry("Введите размер массива: ");
+int[] array = new int[size];
+Fillarray(array);
+Console.Write("[{0}]", string.Join(", ", array));
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма нечетных элемнтов равна {SummNech(array)}");
