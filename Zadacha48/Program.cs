e22b06cdd4c4fc9a8b27e8b101ae2e81c25e7aa2
Oleg <Oleg.evpator@mail.ru>
2022-09-22 13:26:23 +0300

    add Zadacha48

---
+++ Zadacha48/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = 4;
+int m = 3;
+double[,] array = new double[m, n];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
