74c52cca42cdd16e7c5e3914ab9499967abf75be
Oleg <Oleg.evpator@mail.ru>
2022-09-29 12:37:34 +0300

    add Zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите начальное число M: ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Введите начальное число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void Summ (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+ Summ(numberM, numberN, sum);
+}
+ Summ(numberM, numberN, 0);
