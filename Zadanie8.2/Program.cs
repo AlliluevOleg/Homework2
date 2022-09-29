2d2dbbde7d4b2636286aa8e9aafa002877b3150d
Oleg <Oleg.evpator@mail.ru>
2022-09-29 08:15:33 +0300

    add Zadanie8.2

---
+++ Zadanie8.2/Program.cs
@@ -0,0 +1,90 @@
+﻿/* int[,] array1 = new int { { 2, 4 }, { 3, 2 } };
+int[,] array2 = new int { { 3, 4 }, { 3, 3 } };
+
+PrintArray(MultiMatrix(array1, array2));
+int [,] MultiMatrix(int[,] array1, int[,] array2)
+{
+    int result = new int[array1.GetLength(0), array2.GetLength(1)];
+    int sum = 0;
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            sum = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                sum += array1[i ,k] * array2[k, j];
+            }
+            result[i ,j] = sum;
+        }
+    }
+
+}
+void PrintArray(int[,] array)
+{
+    int[] buf = new int[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            buf[j] = array[i, j];
+        }
+        System.Console.WriteLine("[{0}]", string.Join(", ", buf));
+    }
+} */
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); 
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - А");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - В");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
