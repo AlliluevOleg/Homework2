8027a3225f3f3f0b56bc597d5a8700fe4ddc4d92
Oleg <Oleg.evpator@mail.ru>
2022-09-29 11:27:17 +0300

    add Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,13 @@
+﻿int DataEntry(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int n = DataEntry("Введите число: ");
+string Number(int n)
+{
+    if ( n == 1)
+    return n.ToString();
+    return n.ToString() + Number(n-1);
+}
+System.Console.WriteLine(Number(n));
